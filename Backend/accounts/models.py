f2f9e132da67d1712b4b62c1f1d8a7f3e64419e7
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2025-11-07 20:44:37 +0330

    Model CustomUser has been created

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=40)
+    studentNo = models.CharField(max_length=9, blank=True, null=True)
+    phoneNo = models.CharField(max_length=11, blank=True)
+    
+    def __str__(self):
+        return self.usernam
