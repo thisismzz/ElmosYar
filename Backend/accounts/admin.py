5c8df9d66a227d217455f19c0ef3e24a19d5bc4f
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2025-11-07 21:13:03 +0330

    new admin setting added
    CustomUserAdmin added

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra', {'fields': ('studentNo',)})
+    )
+
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ("username", "email", "first_name", "last_name", "studentNo", "password1", "password2")
+        })
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
