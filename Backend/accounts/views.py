d64b51b41ff8e3cb23a88ef5b65d5a4365c8dfd5
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2025-11-07 21:07:36 +0330

    new view added
    signupView added

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login
 
 def signupView(request):
-    pass
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid(): 
+            user = form.save(commit=False)
+            
+            # Approval process
+            # ...
+            
+            user.save()
+            messages.success(request, "Registeration was successful!")
+            # return redirect('')
+    
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'signup.html', {'form' : form})
