bebedcd9a641aaad215d2a9160be3a8723306f4f
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2025-11-22 17:13:39 +0330

    security issue solved

--- Backend/ElmosYar/settings.py
+++ Backend/ElmosYar/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +150,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES' : ('Bearer',),
 }
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
