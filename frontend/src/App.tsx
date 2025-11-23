829a980346120e20cde1db7800a1005e7185451d
omidagha2 <tdmguy.dantdm2@gmail.com>
2025-11-23 18:47:01 +0330

    added postcard and synced with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,10 @@ import { useState, } from 'react';
 import Comments from './components/Comments';
 import { Comment } from './types/comments';
 import './App.css';
-import { getCommentsForPost } from './services/commentService';
+import { getCommentsForPost, getPostCard } from './services/commentService';
 import { stringify } from 'querystring';
+import { Post } from './types/posts';
+import { PostCard } from './components/Posts/PostFeed';
 
 async function loadComments() {
 	const comments = await getCommentsForPost(1);
@@ -15,14 +17,33 @@ function App() {
 	const postId: number = 1;
 
 	const [commentsList, setCommentsList] = useState<Comment[]>([]);
+	const [currentPost, setCurrentPost] = useState<Post>({
+		"comments": 0,
+		"content": "",
+		"id": 0,
+		"user": {
+			avatar: "",
+			id: 0,
+			name: "",
+			username: ""
+		},
+		"timestamp": "",
+		"likes": 0,
+		dislikes: 0,
+	});
 
 	useEffect(() => {
 		async function loadComments() {
 			const result = await getCommentsForPost(postId);
 			setCommentsList(result);
 		}
+		async function loadPostCard() {
+			const result = await getPostCard(postId);
+			setCurrentPost(result);
+		}
 		loadComments();
-
+		loadPostCard();
+		
 	}, [postId]);
 
 	return (
@@ -31,6 +52,12 @@ function App() {
 				<h1>خوش آمدید</h1>
 				<p>سیستم نظردهی</p>
 			</header>
+			<PostCard
+			post={currentPost}
+			onLike={() => {}}
+			onDislike={() => {}}
+			onComment={() => {}}
+			/>
 			<Comments
 				title="نظرات کاربران"
 				comments={commentsList}
