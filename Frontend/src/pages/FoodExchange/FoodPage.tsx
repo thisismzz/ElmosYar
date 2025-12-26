ceb065a9cda104a7a3b3e78c9114a25a89005d15
Setayesh yarijani <152412073+setayeshyarijani@users.noreply.github.com>
2025-12-26 12:25:41 +0330

    some changes in food page color and design

--- Frontend/src/pages/FoodExchange/FoodPage.tsx
+++ Frontend/src/pages/FoodExchange/FoodPage.tsx
@@ -7,7 +7,6 @@ import { postService } from '../../services/PostService';
 import { useFilters } from '../../contexts/FilterContext';
 import { FoodPostSearchProps } from '../../types/food_posts';
 
-
 const FoodPage: React.FC = () => {
 	const [foodItems, setFoodItems] = useState<FoodItem[]>([]);
 	const [loading, setLoading] = useState(true);
