03926b8c9852e1a7894d791bf8327cf8a6ed451f
amir-rahmani84 <seyed.amir.rh1384@gmail.com>
2026-01-01 12:45:33 +0330

    added ContactUs element to the routes
    also deleted some unnecessay variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ import { ProfessorProfilePage } from './pages/Professors/ProfessorProfile';
 import { CreateReviewPage } from './pages/Professors/CreateReview';
 import DiscussionPostForm from './components/Discussion/Posts/DiscussionPostForm';
 import AddFoodModal from './components/Transaction/AddFoodModal';
-
+import ContactUs from './pages/Contact/Contact';
 // PostFeed wrapper components for different routes
 const TopicDiscussion: React.FC = () => {
 
@@ -179,6 +179,7 @@ const ProtectedRoutes: React.FC = () => {
 			<Route path='/profile/wallet' element={<WalletPage />} />
 			<Route path='/profile/edit' element={<EditProfilePage />} />
 			<Route path='/profile/transactions' element={<TransactionHistoryPage />} />
+			<Route path='contactUs' element={<ContactUs />} />
 			<Route path='/topic/:topicId' element={<TopicDiscussion />} />
 			<Route path='/topic/professors/:professorName' element={<ProfessorProfilePage />} />
 			<Route path='/topic/professors/new' element={<CreateReviewPage />} />
